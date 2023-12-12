a826d84cc6f84a8e6ef9fee5c266e70c2ab0c1f9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-12-12 21:31:52 +0100

    refactor: use method reference instead lambda

--- src/main/scala/it/unibo/pps/mvu/runtime/view/TuiElement.scala
+++ src/main/scala/it/unibo/pps/mvu/runtime/view/TuiElement.scala
@@ -22,16 +22,18 @@ object TuiElement:
       userInput match
         case "increment" => Seq(buttonsAndHandlers("increment")())
         case "increment random" => Seq(buttonsAndHandlers("increment random")())
-        case invalidInput @ _ => println(s"Invalid input: '$invalidInput'\n"); Seq.empty
+        case invalidInput @ _ =>
+          println(s"Invalid input: '$invalidInput'\n")
+          Seq[Message]()
 
     private def buttonOnlyElement(rootElement: Element[Message]): Map[String, () => Message] =
       rootElement match
-        case element @ TuiElement.Button(_, _, _) =>
-          element.child.flatMap(el => buttonOnlyElement(el)).toMap + (element.triggerString -> element.onAction)
-        case _ => rootElement.child.flatMap(el => buttonOnlyElement(el)).toMap
+        case element: TuiElement.Button[_] =>
+          element.child.flatMap(buttonOnlyElement).toMap + (element.triggerString -> element.onAction)
+        case _ => rootElement.child.flatMap(buttonOnlyElement).toMap
 
     private def textOnlyElement(rootElement: Element[Message]): Seq[String] =
       rootElement match
-        case elem @ TuiElement.Text(_, _) => elem.text +: elem.child.flatMap(el => textOnlyElement(el))
-        case elem @ TuiElement.Separator(_) => "" +: elem.child.flatMap(el => textOnlyElement(el))
-        case _ => rootElement.child.flatMap(el => textOnlyElement(el))
+        case elem: TuiElement.Text[_] => elem.text +: elem.child.flatMap(textOnlyElement)
+        case elem: TuiElement.Separator[_] => "" +: elem.child.flatMap(textOnlyElement)
+        case _ => rootElement.child.flatMap(textOnlyElement)
