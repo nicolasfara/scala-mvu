e187982dca6cc448c0d014dc41b5bbde6d9c2269
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-12-18 14:32:43 +0100

    build: disable default argument wartg

--- src/main/scala/it/unibo/pps/mvu/runtime/Runtime.scala
+++ src/main/scala/it/unibo/pps/mvu/runtime/Runtime.scala
@@ -5,8 +5,11 @@ import scala.annotation.tailrec
 import it.unibo.pps.mvu.runtime.view.Element
 
 @tailrec
-def runtime[Model, Message, El[A] <: Element[A]](currentModel: Model)(update: (Model, Message) => Model)(view: Model => El[Message])(using er: ElementRenderer[Message, El]): Unit =
+def runtime[Model, Message, El[A] <: Element[A]](currentModel: Model)(
+    update: (Model, Message) => Model
+)(view: Model => El[Message])(using er: ElementRenderer[Message, El]): Unit =
   val rootNode = view(currentModel)
   val messages = er.render(rootNode)
-  val newModel = messages.foldLeft(currentModel)((model, msg) => update(model, msg))
+  val newModel =
+    messages.foldLeft(currentModel)((model, msg) => update(model, msg))
   runtime(newModel)(update)(view)
--- src/main/scala/it/unibo/pps/mvu/runtime/view/Element.scala
+++ src/main/scala/it/unibo/pps/mvu/runtime/view/Element.scala
@@ -12,23 +12,5 @@ trait Element[+Message]:
   val message: Option[Message]
   val child: Seq[Element[Message]]
 
-// trait ElementRenderer[Message, -E[A] <: Element[A]]:
-//   def render(rootElement: E[Message]): Seq[Message]
-
-trait ElementRenderer[Message, -E[A] <: Element[A], ES[Message]]:
-  def render(rootElement: E[Message]): ES[Message]
-
-// given foo: ElementRenderer2[String, TuiElement, [X] =>> Stream[IOException, X]] with
-//   override def render(rootElement: TuiElement[String]): Stream[IOException, String] = ZStream.fromZIO:
-//     for
-//       _ <- Console.printLine("Hello")
-//       input <- Console.readLine
-//     yield "Hello"
-
-// def foob(): Unit = for
-//   queue <- Queue.bounded[String](10)
-//   producer <- foo.render(container[String]{ }).run(ZSink.fromQueue(queue)).forever.fork
-//   consumer <- queue.take.flatMap(Console.printLine(_)).forever.fork
-//   _ <- producer.join
-//   _ <- consumer.join
-// yield ()
+trait ElementRenderer[Message, -E[A] <: Element[A]]:
+  def render(rootElement: E[Message]): IO[IOException, Message]
